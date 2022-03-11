0b8d9e1780fbd2e5471caf8386be88d556ffeb14
Rayssa Vitória de Castro Gonçalves <79856364+rayssadecastro@users.noreply.github.com>
2022-03-11 12:39:27 -0300

    Add files via upload

---
+++ aula_11_03_22/exercicio/media.php
@@ -0,0 +1,9 @@
+<?php
+    $num1 = $_REQUEST['num1'];
+    $num2 = $_REQUEST['num2'];
+    $num3 = $_REQUEST['num3'];
+    $media = ($num1 + $num2 + $num3) /3;
+
+    echo"A media dos numeros é: $media"
+
+?>
